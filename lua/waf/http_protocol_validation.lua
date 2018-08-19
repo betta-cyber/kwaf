a7ffffdcaf1298cfc370a5d0059a9066abc340a0
LightStrawberry <l21828604@gmail.com>
2018-08-20 00:40:48 +0800

    http protocol validation

---
+++ lua/waf/http_protocol_validation.lua
@@ -0,0 +1,85 @@
+require "waf/tools"
+require "resty.core.regex"
+
+local _M = {}
+
+local mt = {  __index = _M }
+local get_headers = ngx.req.get_headers
+local error_code = {
+    "HTTP_BAD_REQUEST": "request could not be understood",
+    "HTTP_METHOD_NOT_ALLOWED": "request method is not allowed",
+    "HTTP_LENGTH_REQUIRED": "request method requires a valid Content-length",
+    "HTTP_REQUEST_ENTITY_TOO_LARGE": "request exceeds system's limit",
+    "HTTP_PROTOCOL_VERSION_FORBIDDEN": "request use forbidden http protocol version",
+    "HTTP_REQUEST_BAD_METHOD": "the requested method is unknown",
+    "HTTP_REQUEST_BAD_URI": "request with invalid uri",
+    "HTTP_REQUEST_BAD_SCHEMA": "request with bad schema",
+    "HTTP_REQUEST_BAD_PROTOCOL": "request with bad protocol version",
+    "HTTP_REQUEST_BAD_CRLF": "request end failed, bad CRLF",
+    "HTTP_RESPONSE_ENTITY_TOO_LARGE": "response exceed system's limit",
+}
+
+local max_uri_length = 4096
+local max_uri_arg_count = 20
+local max_user_agent_length = 1024
+local max_cookie_length = 1024
+local max_cookie_count = 64
+local max_referer_length = 4096
+local max_accept_length = 1024
+local max_accpet_charset_length = 128
+local max_content_length = 10485760
+local max_range_count = 5
+local max_uri_length = 5242880
+local max_http_header_count = 32
+local max_http_header_name_length = 64
+local max_http_header_value_length = 128
+local max_post_arg_count = 256
+local repeat_arg = false
+local repeat_header = true
+local double_url_encode = false
+local abnormal_port = true
+local abnormal_host = true
+
+
+function _M.new()
+    local t = {
+        var = {},
+    }
+    return setmetatable(t, mt)
+end
+
+function _M.check_in_decode()
+    local header = get_headers()
+    local args = ngx.req.get_uri_args()
+    tprint(header)
+    for k,v in pairs(args) do
+        ngx.log(ngx.INFO, "arg: ", k, " value: ", v)
+        if string.find(v, '.*alert') then
+            ngx.log(ngx.INFO, "match")
+            return ngx.exit(ngx.HTTP_NOT_FOUND)
+        end
+    end
+    -- for i = 1, #header do
+        -- ngx.log(ngx.INFO, "header:", header[i])
+    -- end
+    return true
+end
+
+function _M.check_in_strategy()
+    local header = get_headers()
+    local args = ngx.req.get_uri_args()
+    tprint(header)
+    for k,v in pairs(args) do
+        ngx.log(ngx.INFO, "arg: ", k, " value: ", v)
+        if string.find(v, '.*alert') then
+            ngx.log(ngx.INFO, "match")
+            return ngx.exit(ngx.HTTP_NOT_FOUND)
+        end
+    end
+    -- for i = 1, #header do
+        -- ngx.log(ngx.INFO, "header:", header[i])
+    -- end
+    return true
+end
+
+return _M
--- lua/waf/web_general_sec.lua
+++ lua/waf/web_general_sec.lua
@@ -1,4 +1,6 @@
-require 'waf/tools'
+require "waf/tools"
+require "resty.core.regex"
+
 local _M = {}
 
 local mt = {  __index = _M }
@@ -14,8 +16,18 @@ end
 function _M.load_secrules(ruleset, opts)
     local a = 1
     local header = get_headers()
+    local args = ngx.req.get_uri_args()
     tprint(header)
-    ngx.log(ngx.INFO, "header:", header)
+    for k,v in pairs(args) do
+        ngx.log(ngx.INFO, "arg: ", k, " value: ", v)
+        if string.find(v, '.*alert') then
+            ngx.log(ngx.INFO, "match")
+            return ngx.exit(ngx.HTTP_NOT_FOUND)
+        end
+    end
+    -- for i = 1, #header do
+        -- ngx.log(ngx.INFO, "header:", header[i])
+    -- end
     return true
 end
 
