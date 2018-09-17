fd6c3ea17904b695cf6d6dc51f8bc8be22ea99f0
LightStrawberry <l21828604@gmail.com>
2018-09-18 03:13:25 +0800

    add multipart parser

--- lua/waf.lua
+++ lua/waf.lua
@@ -1,12 +1,10 @@
 local config = require 'config'
 local web_general_sec = require 'waf/web_general_sec'
 local http_protocol_validation = require 'waf/http_protocol_validation'
-local upload_limit = require 'waf/upload_limit' 
+local upload_limit = require 'waf/upload_limit'
 
 -- local content_length = tonumber(ngx.req.get_headers()['content-length'])
--- local method = ngx.req.get_method()
-
-print(config.waf_enable)
+local method = ngx.req.get_method()
 
 if config.waf_enable then
     if config.http_protocol_validation then
@@ -17,9 +15,12 @@ if config.waf_enable then
         local web_general_sec = web_general_sec:new()
         web_general_sec:check()
     end
-    if config.upload_limit then
-        local upload_limit = upload_limit:new()
-        upload_limit:check()
+    -- post need check file upload
+    if method == 'POST' then
+        if config.upload_limit then
+            local upload_limit = upload_limit:new()
+            upload_limit:check()
+        end
     end
     -- todo:
     -- white list
--- lua/waf/upload_limit.lua
+++ lua/waf/upload_limit.lua
@@ -2,11 +2,12 @@ local tools = require "waf/tools"
 local regex = require "resty.core.regex"
 local cjson = require "cjson";
 local engine = require "waf/waf_engine"
+local parser = require "lib/parser"
 
 local _M = {}
 
 local mt = {  __index = _M }
-local get_headers = ngx.req.get_headers
+local rulematch = ngx.re.find
 
 function _M.new()
     local t = {
@@ -17,10 +18,33 @@ end
 
 function _M.upload_check(self)
     ngx.req.read_body()
-    local data = ngx.req.get_body_data()
-    print(data)
+    local body = ngx.req.get_body_data()
+    local p, err = parser.new(body, ngx.var.http_content_type)
+    if not p then
+        ngx.log(ngx.ERROR, "failed to create parser: "..err)
+        return
+    end
+    -- load upload file
+    while true do
+        local file = p:parse_part()
+        if not file then
+            break
+        end
+        tprint(file)
+        if self:php_check(file['part_body']) then
+            return true
+        end
+    end
     return false
 end
+
+function _M.php_check(self, part_body)
+    -- match php start tag
+    if rulematch(part_body, '<\\?[\\s\\n\\b]*php', "jo") then
+        return true
+    end
+end
+    
 
 -- main function
 function _M.check(self)
