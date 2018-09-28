751f44eb2742091fd1680e1a5adb53266e30653d
betta <l21828604@gmail.com>
2018-09-28 11:53:01 +0800

    merge

--- lua/config.lua
+++ lua/config.lua
@@ -2,9 +2,9 @@ local _M = {
     --waf status
     waf_enable = true,
     --log dir
-    log_dir = "/Users/shokill/kwaf/logs",
+    log_dir = "/home/betta/kwaf/logs",
     --rule setting
-    rule_dir = "/Users/shokill/kwaf/rule",
+    rule_dir = "/home/betta/kwaf/rule",
     --enable/disable http_protocol_validation
     http_protocol_validation = true,
     --enable/disable web_general_sec
@@ -13,6 +13,12 @@ local _M = {
     upload_limit = true,
     --enable/disable sensitive_info
     sensitive_info = true,
+    --redis config
+    -- redis_host = "127.0.0.1",
+    --redis port
+    -- redis_port = 6379,
+    --redis pass
+    -- redis_pass = ""
 }
 
 return _M
